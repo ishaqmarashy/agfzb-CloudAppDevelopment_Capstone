58740bb0c88968dabab97285a8e741c314ef15d8
ishaqmarashy <34040118+ishaqmarashy@users.noreply.github.com>
2023-04-07 14:14:37 +0400

    Add Signup, Auth, Login, Logout

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,24 +10,27 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='about', view=views.about, name='about'),
+    path('about/', views.about, name='about'),
 
     # path for contact us view
-    path(route='contact', view=views.contact, name='contact'),
+    path('contact/', views.contact, name='contact'),
 
     # path for registration
-    path(route='registration', view=views.registration_request, name='registration'),
+    path('register/', views.registration_request, name='registration'),
 
     # path for login
-    path(route='login', view=views.login, name='login'),
+    path('login/', views.login_request, name='login'),
 
     # path for logout
-    path(route='', view=views.get_dealerships, name='index'),
+    path('logout/', views.logout_request, name='logout'),
+
+    # path for index view
+    path('', views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-    path(route='dealer', view=views.get_dealerships, name='dealer'),
+    path('dealer/', views.get_dealerships, name='dealer'),
 
     # path for add a review view
-    path(route='review', view=views.add_review, name='review'),
+    path('review/', views.add_review, name='review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
 # from .restapis import related methods
@@ -34,19 +35,40 @@ def contact(request):
 def login_request(request):
     context = {}
     if request.method == "POST":
+        username = request.POST.get('user')
+        password = request.POST.get('psw')
+        user = authenticate(request, username=username, password=password)
+        next_url = request.GET.get('next', None)
+        if not next_url:
+            next_url=reverse('djangoapp:index')
+        if user is not None:
+            login(request, user)
+            return redirect(f'{next_url}?success=True')
+        else:
+            return redirect(f'{next_url}?success=False')
         return render(request, request.path , context)
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-    context = {}
-    if request.method == "POST":
-        return render(request, request.path , context)
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 def registration_request(request):
-    context = {}
     if request.method == "GET":
-        return render(request,  'djangoapp/registration.html' , context)
+        return render(request,  'djangoapp/registration.html')
+    if request.method == 'POST':
+        if User.objects.filter(username=request.POST['user']).exists():
+            # User with this username already exists
+            return redirect(f'/djangoapp/register?success=False')
+        else:
+            user = User.objects.create_user(username=request.POST['user'],
+                                            password=request.POST['psw'],
+                                            first_name=request.POST['firstname'],
+                                            last_name=request.POST['lastname'])
+            print(request.POST['user'], 'registered')
+        return redirect(f'/djangoapp/register?success=True')
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
